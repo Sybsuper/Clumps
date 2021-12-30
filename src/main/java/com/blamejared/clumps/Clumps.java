661a9f1f85d01155ee5a1d819447d875811b8252
Sybsuper <sybsuper@gmail.com>
2021-12-30 19:06:12 +0100

    Actual Fix Concurrent Modification

--- src/main/java/com/blamejared/clumps/Clumps.java
+++ src/main/java/com/blamejared/clumps/Clumps.java
@@ -2,7 +2,8 @@ package com.blamejared.clumps;
 
 import com.blamejared.clumps.entities.EntityXPOrbBig;
 import com.blamejared.clumps.events.EXPCloneEvent;
-import net.minecraft.entity.*;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.item.ExperienceOrbEntity;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
@@ -17,19 +18,22 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Mod(Clumps.MODID)
 public class Clumps {
-    
+
     public static final String MODID = "clumps";
 
     private static final DeferredRegister<EntityType<?>> DEFERRED_REGISTER = DeferredRegister.create(ForgeRegistries.ENTITIES, MODID);
     public static final RegistryObject<EntityType<EntityXPOrbBig>> BIG_ORB_ENTITY_TYPE = DEFERRED_REGISTER.register("xp_orb_big", () -> EntityType.Builder.<EntityXPOrbBig>create(EntityXPOrbBig::new, EntityClassification.MISC).size(0.5f, 0.5f).build(MODID + ":xp_orb_big"));
-    
+    private static final List<ExperienceOrbEntity> orbs = new ArrayList<>();
+
     public Clumps() {
         final IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus(),
                 forgeBus = MinecraftForge.EVENT_BUS;
@@ -39,23 +43,24 @@ public class Clumps {
         modBus.addListener(this::setup);
         forgeBus.addListener(this::joinWorld);
         forgeBus.addListener(this::update);
-    
-    }
 
-    private static final List<ExperienceOrbEntity> orbs = new ArrayList<>();
+    }
 
     private void setup(FMLCommonSetupEvent e) {
         ObfuscationReflectionHelper.setPrivateValue(EntityType.class, BIG_ORB_ENTITY_TYPE.get(), EntityType.EXPERIENCE_ORB.getTranslationKey(), "field_210762_aX");
     }
-    
+
     private void update(TickEvent.WorldTickEvent e) {
-        if(e.world.isRemote || e.phase == TickEvent.Phase.START) {
+        if (e.world.isRemote || e.phase == TickEvent.Phase.START) {
             return;
         }
-        if(e.world instanceof ServerWorld && !orbs.isEmpty()) {
-            Iterator<ExperienceOrbEntity> it = orbs.iterator();
-            while(it.hasNext()) {
-                ExperienceOrbEntity entity = it.next();
+        if (e.world instanceof ServerWorld && !orbs.isEmpty()) {
+            List<ExperienceOrbEntity> toremove = new ArrayList<>();
+            List<ExperienceOrbEntity> orbscopy;
+            synchronized (orbs) {
+                orbscopy = new ArrayList<>(orbs);
+            }
+            for (ExperienceOrbEntity entity : orbscopy) {
                 HashMap<Integer, Long> clumpedMap = new HashMap<>();
                 clumpedMap.put(entity.xpValue, 1L);
                 EntityXPOrbBig bigOrb = new EntityXPOrbBig(entity.getEntityWorld(), entity.getPosX(), entity.getPosY(), entity.getPosZ(), entity.xpValue, clumpedMap);
@@ -64,17 +69,25 @@ public class Clumps {
                 bigOrb.setMotion(entity.getMotion());
                 entity.getEntityWorld().addEntity(bigOrb);
                 entity.remove();
+                toremove.add(entity);
+            }
+            synchronized (orbs) {
+                for (ExperienceOrbEntity experienceOrbEntity : toremove) {
+                    orbs.remove(experienceOrbEntity);
+                }
             }
         }
     }
-    
+
     private void joinWorld(EntityJoinWorldEvent e) {
-        if(e.getEntity() instanceof ExperienceOrbEntity && e.getEntity().getType() != BIG_ORB_ENTITY_TYPE.get()) {
+        if (e.getEntity() instanceof ExperienceOrbEntity && e.getEntity().getType() != BIG_ORB_ENTITY_TYPE.get()) {
             World world = e.getEntity().world;
-            if(!world.isRemote) {
-                orbs.add((ExperienceOrbEntity) e.getEntity());
+            if (!world.isRemote) {
+                synchronized (orbs) {
+                    orbs.add((ExperienceOrbEntity) e.getEntity());
+                }
             }
         }
     }
-    
+
 }
